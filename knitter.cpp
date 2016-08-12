e03c212f1b071a80254fca0879fd7c64a375cce1
christiansiegel <christiansiegel@users.noreply.github.com>
2016-08-12 19:26:25 +0200

    added FADE and MIN_DISTANCE parameters

--- knitter.cpp
+++ knitter.cpp
@@ -7,19 +7,22 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 // Image's filename. Should be a square image.
-const std::string IMAGE = "image.png";
+const std::string IMAGE = "image.jpg";
 
-// Diameter of the circle in mm
-const unsigned int CIRCLE_DIAMETER = 600;
-
-// Diameter of the string in mm
-const float STRING_DIAMETER = 0.3;
+// Size of image in pixels
+const unsigned int SIZE = 700;
 
 // Number of hooks
 const unsigned int NR_HOOKS = 200;
 
 // Number of strings
-const unsigned int NR_STRINGS = 1500;
+const unsigned int NR_STRINGS = 4000;
+
+// Reduce color (0-255) by this value if string runs through
+const unsigned int FADE = 30;
+
+// Minimal distance between two consecutive pins
+const unsigned int MIN_DISTANCE = 20;
 
 // Returns the coordinates of the circular hooks based on their number, the
 // circle's center and radius.
@@ -62,22 +65,24 @@ std::vector<cv::Point> linePixels(cv::Point a, cv::Point b) {
 
 // Returns the score of a line from a to b, based on the image's pixels it
 // passes through.
-unsigned long lineScore(cv::Mat &image, cv::Point a, cv::Point b) {
+double lineScore(cv::Mat &image, cv::Point a, cv::Point b) {
   unsigned long score = 0;
   std::vector<cv::Point> points = linePixels(a, b);
   for (auto &p : points) {
     // linear; black pixel gets maximum score of 255
     score += 0xff - image.at<uchar>(p.y, p.x);
   }
-  return score;
+  return (double)score / points.size();
 }
 
 // Reduce darkness of image's pixels the line from a to b passes through.
 void reduceLine(cv::Mat &image, cv::Point a, cv::Point b) {
   std::vector<cv::Point> points = linePixels(a, b);
-  for (auto &p : points) {
-    // maximum reduction to white
-    image.at<uchar>(p.y, p.x) = 0xff;
+  for (auto &p : points) {cd
+    unsigned int color = image.at<uchar>(p.y, p.x);
+    color += FADE;
+    if (color > 0xff) color = 0xff;
+    image.at<uchar>(p.y, p.x) = color;
   }
 }
 
@@ -85,10 +90,14 @@ void reduceLine(cv::Mat &image, cv::Point a, cv::Point b) {
 // maximum score.
 unsigned int nextHook(unsigned int current, std::vector<cv::Point> hooks,
                       cv::Mat &mat) {
-  unsigned long maxScore = 0;
+  double maxScore = 0;
   unsigned int next = 0;
   for (unsigned int i = 0; i < hooks.size(); ++i) {
-    unsigned long score = lineScore(mat, hooks.at(current), hooks.at(i));
+    // Prevent two consecutive pins with less than minimal distance
+    unsigned int diff = (unsigned int)std::abs((int)current - (int)i);
+    if(diff < MIN_DISTANCE || diff > NR_HOOKS - MIN_DISTANCE) continue;
+    // Calculate line score
+    double score = lineScore(mat, hooks.at(current), hooks.at(i));
     if (score > maxScore) {
       maxScore = score;
       next = i;
@@ -105,18 +114,16 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  // Calculate size of the image, so that the string has a diameter of 1 pixel.
-  int size = CIRCLE_DIAMETER / STRING_DIAMETER;
-  cv::Point center = cv::Point(size / 2, size / 2);
-  std::vector<cv::Point> hooks = calcHooks(NR_HOOKS, center, size / 2);
+  cv::Point center = cv::Point(SIZE / 2, SIZE / 2);
+  std::vector<cv::Point> hooks = calcHooks(NR_HOOKS, center, SIZE / 2);
 
   // Resize the original image and convert it to grayscale.
   cv::Mat img;
   cv::cvtColor(original, img, cv::COLOR_RGB2GRAY);
-  cv::resize(img, img, cv::Size(size, size));
+  cv::resize(img, img, cv::Size(SIZE, SIZE));
 
   // Image with the simulated result.
-  cv::Mat result(size, size, CV_8U, 255);
+  cv::Mat result(SIZE, SIZE, CV_8U, 255);
 
   // Output windows
   cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
@@ -128,6 +135,7 @@ int main(int argc, char **argv) {
     // Get next hook
     unsigned int next = nextHook(current, hooks, img);
     std::cout << "String #" << i << " -> next hook: " << next << std::endl;
+    //std::cout << next << ",";
 
     // Update resulting image and reduce darkness in original image
     reduceLine(img, hooks.at(current), hooks.at(next));
